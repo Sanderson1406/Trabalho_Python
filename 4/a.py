a4da8272a96c1ad818af981e94f7766fe259f1f3
Sanderson Machado <sandersonoficial10@hotmail.com>
2023-06-09 13:23:52 -0300

    CONCLUÍDO

---
+++ 4/a.py
@@ -0,0 +1,8 @@
+numero = 5
+
+if numero == 0:
+    print('O número é zero')
+elif numero > 0:
+    print('O número é positivo')
+else:
+    print('O número é negativo')
---
+++ 4/b.py
@@ -0,0 +1,9 @@
+numero = 7
+
+if numero > 0 and numero < 10:
+    if numero%2 == 0:
+        print('O número é par, positivo e menor que 10')
+    else:
+        print('O número não atende aos critérios!')
+else:
+    print('O número é menor que 0 ou maior que 10')
